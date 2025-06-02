b8f1a1555654cb3e4c6bf232385feea06718f1cc
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-02 19:44:55 +0300

    feat: add Register route and move Navbar inside ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Home from './pages/Home'
 import Navbar from './components/UI/Navbar'
 import Login from './pages/Login'
 import ProtectedRoute from './components/ProtectedRoute'
+import Register from './pages/Register'
 
 function App() {
 
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/"
           element={
             <ProtectedRoute>
+              <Navbar />
               <Home />
             </ProtectedRoute>
           }
