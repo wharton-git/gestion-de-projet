a366b4d67acfbc35978a4879d831a6c96acb124b
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-02 20:46:55 +0300

    Re : init

--- src/services/project.js
+++ src/services/project.js
@@ -13,3 +13,17 @@ export const createProject = async (project) => {
     })
     return await res.json()
 }
+
+export async function deleteProject(id) {
+    const res = await fetch(`http://localhost:3000/projects/${id}`, { method: 'DELETE' })
+    if (!res.ok) throw new Error('Erreur suppression projet')
+}
+
+export async function updateProject(id, data) {
+    const res = await fetch(`http://localhost:3000/projects/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    })
+    return await res.json()
+}
