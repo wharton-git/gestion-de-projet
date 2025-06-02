ec72cbdff2ea6a553077bff1899a53dbdcfd0cfa
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-02 20:25:59 +0300

    Initialize users and projects data in the database

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProjectForm from './../components/ProjectForm'
+import ProjectList from './../components/ProjectList'
 
 const Home = () => {
+    const [refreshKey, setRefreshKey] = useState(0)
+
+    const handleProjectCreated = () => {
+        setRefreshKey((prev) => prev + 1)
+    }
+
     return (
-        <div>Home</div>
+        <div className="max-w-3xl mx-auto mt-10 space-y-6">
+            <ProjectForm onProjectCreated={handleProjectCreated} />
+            <ProjectList refresh={refreshKey} />
+        </div>
     )
 }
 
