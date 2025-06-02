730a96cdee61a0ad048abebd700c6ca111473b21
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-02 19:17:32 +0300

    Update routing in App component and implement ProtectedRoute for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './components/Home'
 import Navbar from './components/UI/Navbar'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   )
