0cb26a3d6cc633ce9276d19796d6666b608f1de0
Xeon-Shinigami <whartonaldrick@gmail.com>
2025-06-04 01:15:49 +0300

    Fix: Update favicon and title in index.html; enhance Navbar with user dropdown menu

--- src/components/UI/Navbar.jsx
+++ src/components/UI/Navbar.jsx
@@ -12,19 +12,28 @@ const Navbar = () => {
     }
 
     return (
-        <div className="navbar bg-base-100 shadow-sm fixed">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Project Manager</a>
-            </div>
-            {user && (
-                <div className="flex items-center gap-4">
-                    <span className="text-sm text-gray-500"> {user.username}</span>
-                    <button className="btn btn-outline btn-error btn-sm" onClick={handleLogout}>
-                        Logout
-                    </button>
+        <>
+            <div className="navbar bg-base-100 shadow-sm fixed">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">Project Manager</a>
                 </div>
-            )}
-        </div>
+                <div className="flex gap-2">
+                    {user && (<div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar avatar-placeholder">
+                            <div className="bg-neutral text-neutral-content w-12 rounded-full">
+                                <div>{user.username && user.username[0].toUpperCase()}</div>
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li><a>Settings</a></li>
+                            <li><a onClick={handleLogout}>Logout</a></li>
+                        </ul>
+                    </div>)}
+                </div>
+            </div>
+        </>
     )
 }
 
